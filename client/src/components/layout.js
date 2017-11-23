805911ad8a187377db8253b2398b0562b3f50f57
Randie Bemis <randie.bemis@gmail.com>
2017-11-23 12:56:35 -0800

    make app header menu

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Container, Menu, Image } from 'semantic-ui-react';
+
+const menuStyle = {
+  height: '3.4rem',
+};
+
+const imageStyle = {
+  width: '40px',
+  marginRight: '1rem',
+};
+
+const logoStyle = {
+  fontSize: '1.2rem',
+  fontWeight: '500',
+  fontFamily: 'verdana',
+  border: 'none',
+};
+
+const contentStyle = {
+  marginTop: '6rem',
+};
 
 export default props => (
-  <div className="container">
-    <header>readable</header>
-    <div className="content">{props.children}</div>
-    <footer>footer</footer>
+  <div>
+    <Menu fixed="top" style={menuStyle}>
+      <Container>
+        <Menu.Item style={logoStyle}>
+          <Image size="mini" src="/readable-logo.svg" style={imageStyle} />
+          readable
+        </Menu.Item>
+        <Menu.Item as="a">All</Menu.Item>
+        <Menu.Item as="a">React</Menu.Item>
+        <Menu.Item as="a">Redux</Menu.Item>
+        <Menu.Item as="a">Udacity</Menu.Item>
+      </Container>
+    </Menu>
+    <Container style={contentStyle}>{props.children}</Container>
   </div>
 );
