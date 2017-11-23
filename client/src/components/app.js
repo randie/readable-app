81201654f02e6cd4c1efaf215235397060e3bdb9
Randie Bemis <randie.bemis@gmail.com>
2017-11-23 15:05:53 -0800

    create redux store for app

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './layout';
+import Store from './store';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Router>
-          <Switch>
-            <Route exact path="/" render={this.homePage} />
-            <Route render={this.notFoundPage} />
-          </Switch>
-        </Router>
+        <Store>
+          <Router>
+            <Switch>
+              <Route exact path="/" render={this.homePage} />
+              <Route path="*" render={this.notFoundPage} />
+            </Switch>
+          </Router>
+        </Store>
       </Layout>
     );
   }
