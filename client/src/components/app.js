95689ff9bb62701dc9180f7418b2c8efa7b28ece
Randie Bemis <randie.bemis@gmail.com>
2017-11-25 17:22:32 -0800

    WIP refactor layout and homepage

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Store from './store';
+import Layout from './layout';
 import HomePage from './home-page';
 import NotFoundPage from './not-found-page';
 
 export default () => (
   <Store>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/:category" component={HomePage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <Layout>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/:category" component={HomePage} />
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+      </Router>
+    </Layout>
   </Store>
 );
--- client/src/components/home-page.js
+++ client/src/components/home-page.js
@@ -1,65 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { Card, Icon } from 'semantic-ui-react';
-import Layout from './layout';
 import { fetchPosts } from '../actions';
+import {
+  Button,
+  Card,
+  Container,
+  Dropdown,
+  Icon,
+  Menu,
+} from 'semantic-ui-react';
 
 class HomePage extends Component {
-  selectCategory = location => {
-    this.props.history.push(location);
-  };
+  state = { activeItem: 'home' };
 
-  postCards = () => {
-    const { posts } = this.props;
-    return (
-      posts &&
-      posts.length && (
-        <Card.Group itemsPerRow={3}>
-          {posts.map(post => (
-            <Card centered key={post.id}>
-              <Card.Content>
-                <Card.Header>{post.title}</Card.Header>
-                <Card.Meta>{moment(post.timestamp).format('ll LT')}</Card.Meta>
-              </Card.Content>
-              <Card.Content description={post.body} />
-              <Card.Content>
-                <span>
-                  <Icon disabled name="user" />
-                  {post.author}
-                </span>
-                <span>
-                  <Icon disabled name="tag" />
-                  {post.category}
-                </span>
-                <span>
-                  <Icon disabled name="thumbs up" />
-                  {post.voteScore}
-                </span>
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
-      )
-    );
-  };
-
-  componentDidMount() {
-    this.props.fetchPosts();
-  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const selectedCategory = this.props.match.params.category;
+    const { activeItem } = this.state;
+
     return (
-      <Layout
-        selectedCategory={selectedCategory}
-        onSelectCategory={this.selectCategory}
-      >
-        {this.postCards()}
-      </Layout>
+      <div>
+        <Menu size="tiny">
+          <Menu.Item
+            name="All"
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="react"
+            active={activeItem === 'react'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="redux"
+            active={activeItem === 'redux'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="udacity"
+            active={activeItem === 'udacity'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="javascript"
+            active={activeItem === 'javascript'}
+            onClick={this.handleItemClick}
+          />
+
+          <Menu.Menu position="right">
+            <Dropdown item text="Sort by">
+              <Dropdown.Menu>
+                <Dropdown.Item disabled>author</Dropdown.Item>
+                <Dropdown.Item>date</Dropdown.Item>
+                <Dropdown.Item disabled>title</Dropdown.Item>
+                <Dropdown.Item>votes</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Item>
+              <Button primary>New Post</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
     );
   }
 }
+//<Button content="New Post" icon="add" labelPosition="left" primary />;
 
 const mapStateToProps = ({ posts }) => ({ posts });
 const mapDispatchToProps = dispatch => ({
