b733eac91259fac0dd126b275f5c7df246a7287a
Randie Bemis <randie.bemis@gmail.com>
2017-11-26 00:27:44 -0800

    fetch all posts for refactored home page

--- client/src/components/home-page.js
+++ client/src/components/home-page.js
@@ -11,6 +11,9 @@ import {
   Menu,
 } from 'semantic-ui-react';
 
+const menuBarStyle = {
+  marginBottom: '3rem',
+};
 class HomePage extends Component {
   state = { activeItem: 'all' };
 
@@ -19,53 +22,95 @@ class HomePage extends Component {
     this.props.history.push(location);
   };
 
-  componentDidMount() {
-    this.props.fetchCategories();
-  }
-
-  render() {
+  menuBar = () => {
     const { activeItem } = this.state;
     const { categories } = this.props;
 
     return (
-      <div>
-        <Menu size="tiny">
+      <Menu size="tiny" style={menuBarStyle}>
+        <Menu.Item
+          name="all"
+          active={activeItem === 'all'}
+          onClick={this.handleItemClick('/')}
+        />
+        {categories.map(category => (
           <Menu.Item
-            name="all"
-            active={activeItem === 'all'}
-            onClick={this.handleItemClick('/')}
+            name={category.name}
+            active={category.name === activeItem}
+            onClick={this.handleItemClick(`/${category.path}`)}
+            key={category.name}
           />
-          {categories.map(category => (
-            <Menu.Item
-              name={category.name}
-              active={category.name === activeItem}
-              onClick={this.handleItemClick(`/${category.path}`)}
-              key={category.name}
-            />
+        ))}
+
+        <Menu.Menu position="right">
+          <Dropdown item text="Sort by">
+            <Dropdown.Menu>
+              <Dropdown.Item disabled>author</Dropdown.Item>
+              <Dropdown.Item>date</Dropdown.Item>
+              <Dropdown.Item disabled>title</Dropdown.Item>
+              <Dropdown.Item>votes</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Menu.Item>
+            <Button primary>New Post</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  };
+
+  postCards = () => {
+    const { posts } = this.props;
+    return (
+      posts &&
+      posts.length && (
+        <Card.Group itemsPerRow={3}>
+          {posts.map(post => (
+            <Card centered key={post.id}>
+              <Card.Content>
+                <Card.Header>{post.title}</Card.Header>
+                <Card.Meta>{moment(post.timestamp).format('ll LT')}</Card.Meta>
+              </Card.Content>
+              <Card.Content description={post.body} />
+              <Card.Content>
+                <span>
+                  <Icon disabled name="user" />
+                  {post.author}
+                </span>
+                <span>
+                  <Icon disabled name="tag" />
+                  {post.category}
+                </span>
+                <span>
+                  <Icon disabled name="thumbs up" />
+                  {post.voteScore}
+                </span>
+              </Card.Content>
+            </Card>
           ))}
+        </Card.Group>
+      )
+    );
+  };
 
-          <Menu.Menu position="right">
-            <Dropdown item text="Sort by">
-              <Dropdown.Menu>
-                <Dropdown.Item disabled>author</Dropdown.Item>
-                <Dropdown.Item>date</Dropdown.Item>
-                <Dropdown.Item disabled>title</Dropdown.Item>
-                <Dropdown.Item>votes</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+  componentDidMount() {
+    this.props.fetchCategories();
+    this.props.fetchPosts();
+  }
 
-            <Menu.Item>
-              <Button primary>New Post</Button>
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
+  render() {
+    return (
+      <div>
+        {this.menuBar()}
+        {this.postCards()}
       </div>
     );
   }
 }
-//<Button content="New Post" icon="add" labelPosition="left" primary />;
 
 const mapStateToProps = ({ categories, posts }) => ({ categories, posts });
+
 const mapDispatchToProps = dispatch => ({
   fetchCategories: () => dispatch(fetchCategories()),
   fetchPosts: () => dispatch(fetchPosts()),
