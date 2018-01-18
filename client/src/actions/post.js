762df328305db91d629f58b84376fbebdf8ad22d
Randie Bemis <randie.bemis@gmail.com>
2018-01-18 00:54:19 -0800

    move create post action and reducer from posts to post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,8 +1,17 @@
+import uuidv4 from 'uuid/v4';
 import api from '../utils/api';
 
+export const CREATE_POST = 'CREATE_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const FETCH_POST = 'FETCH_POST';
 export const VOTE_FOR_POST = 'VOTE_FOR_POST';
+
+export function createPostAction(post) {
+  const url = '/posts';
+  const data = { id: uuidv4(), timestamp: Date.now(), ...post };
+  const payload = api.post(url, data).then(response => response.data);
+  return { type: CREATE_POST, payload };
+}
 
 export function deletePostAction(postId) {
   const url = `/posts/${postId}`;
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,13 @@
 import { orderBy } from 'lodash';
-import { CREATE_POST, FETCH_POSTS, SORT_POSTS } from '../actions';
+import { FETCH_POSTS, SORT_POSTS } from '../actions';
 
 // redux-promise-middleware returns a new action with "_FULFILLED"
 // tacked on to the original action type when the promise for a
 // payload is fulfilled.
-const CREATE_POST_FULFILLED = `${CREATE_POST}_FULFILLED`;
 const FETCH_POSTS_FULFILLED = `${FETCH_POSTS}_FULFILLED`;
 
 function posts(state = [], action) {
   switch (action.type) {
-    case CREATE_POST_FULFILLED:
-      return orderBy([action.payload, ...state], ['timestamp'], ['desc']);
-
     case FETCH_POSTS_FULFILLED:
       return orderBy(action.payload, ['timestamp'], ['desc']);
 
